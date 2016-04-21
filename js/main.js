a83c5c8ef04f9ad226277b9395113e54d7ab0729
Nasrine <azar0006@algonquinlive.com>
2016-04-21 14:43:33 -0400

    applied way points to candies, not candies and candy ghost

---
+++ js/main.js
@@ -0,0 +1,29 @@
+var $candies = $('.candies');
+var $notcandies = $('.notcandies');
+var $candyghost = $('.candyghost');
+
+$candies.waypoint(function (direction) {
+  if (direction == 'down') {
+    $candies.addClass('opacity')
+  } else {
+    $candies.removeClass('opacity')
+  }
+}, {offset:'30%'});
+
+$notcandies.waypoint(function (direction) {
+  if (direction == 'down') {
+    $notcandies.addClass('opacity')
+
+  } else {
+    $notcandies.removeClass('opacity')
+  }
+}, {offset:'30%'});
+
+$candyghost.waypoint(function (direction) {
+  if (direction == 'down') {
+    $candyghost.addClass('candyghostappears')
+
+  } else {
+    $candyghost.removeClass('candyghostappears')
+  }
+}, {offset:'90%'});
